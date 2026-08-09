84b87f09c18e383c26b1bbb0464ac66891b5666e
Rajesh Gollapudi <rajesh.gollapudi@3ds.com>
2026-08-09 09:02:02 -0400

    chore: add missing links to events in photo gallery page

--- app/routes/gallery.tsx
+++ app/routes/gallery.tsx
@@ -41,108 +41,238 @@ export default function GalleryPage() {
         <TabsContent value="pictures">
           <ul className="prose dark:prose-invert">
             <li>
-              <a href="https://flic.kr/s/aHBqjC8Z2S">Holi 2025</a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.flickr.com/photos/155699555@N08/albums/72177720332578306"
+              >
+                Hum Sub Apex Holi 2026 (Ishaan Pandey)
+              </a>
             </li>
             <li>
-              <a href="https://flic.kr/s/aHBqjC8m6g">Basant Bahar 2025</a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.flickr.com/photos/155699555@N08/albums/72177720332517319"
+              >
+                Basant Bahar 2026 (Christine Harrigan)
+              </a>
             </li>
             <li>
-              <a href="https://photos.app.goo.gl/YjXYubCGCdYyfQ4b9">YAA Luncheon 2024</a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.flickr.com/photos/155699555@N08/albums/72177720330952852"
+              >
+                Hum Sub Triangle Got Talent Dec 2025 (Awe Raw Photography)
+              </a>
             </li>
             <li>
-              <a href="https://drive.google.com/drive/folders/1m_ddmbgVrZrToUh-79_Xn5wZuqffL3Bt?usp=sharing">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.flickr.com/photos/155699555@N08/albums/72177720329823545"
+              >
+                Hum Sub Diwali 2025 (Awe Raw Photography)
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.flickr.com/photos/155699555@N08/albums/72177720329852379"
+              >
+                Hum Sub Diwali 2025 - John Tal
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.flickr.com/photos/155699555@N08/albums/72177720329076602"
+              >
+                NC State Capitol Event
+              </a>
+            </li>
+            <li>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjC8Z2S">
+                Holi 2025
+              </a>
+            </li>
+            <li>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjC8m6g">
+                Basant Bahar 2025
+              </a>
+            </li>
+            <li>
+              <a target="_blank" rel="noopener noreferrer" href="https://photos.app.goo.gl/YjXYubCGCdYyfQ4b9">
+                YAA Luncheon 2024
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://drive.google.com/drive/folders/1m_ddmbgVrZrToUh-79_Xn5wZuqffL3Bt?usp=sharing"
+              >
                 Hum Sub Diwali 2024
               </a>
             </li>
             <li>
-              <a href="https://hum-sub-basant-bahar.pictogenix.com/">Basant Bahar 2024</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://hum-sub-basant-bahar.pictogenix.com/">
+                Basant Bahar 2024
+              </a>
             </li>
             <li>
-              <a href="https://flic.kr/s/aHBqjB2U7L">YAA Luncheon 2023</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjB2U7L">
+                YAA Luncheon 2023
+              </a>
             </li>
             <li>Hum Sub Diwali 2023:</li>
             <ul>
               <li>
-                <a href="https://flic.kr/s/aHBqjAYDAn">John Tal Collection</a>
+                <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjAYDAn">
+                  John Tal Collection
+                </a>
               </li>
               <li>
-                <a href="https://flic.kr/s/aHBqjB31Wb">ABFotos Collection</a>
+                <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjB31Wb">
+                  ABFotos Collection
+                </a>
               </li>
             </ul>
             <li>
-              <a href="https://flic.kr/s/aHBqjATEX3">Lazy Daze - Aug 2023(Partner Event)</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjATEX3">
+                Lazy Daze - Aug 2023(Partner Event)
+              </a>
             </li>
             <li>
-              <a href="https://flic.kr/s/aHBqjAEzGx">Cultural Festival 2023 @TMSA (Partner Event)</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjAEzGx">
+                Cultural Festival 2023 @TMSA (Partner Event)
+              </a>
             </li>
             <li>
-              <a href="https://flic.kr/s/aHBqjAEzBY">Sponsorship Dinner 2023</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjAEzBY">
+                Sponsorship Dinner 2023
+              </a>
             </li>
             <li>
-              <a href="https://flic.kr/s/aHBqjABPpu">EarthFest 2023 (Partner Event)</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjABPpu">
+                EarthFest 2023 (Partner Event)
+              </a>
             </li>
             <li>
-              <a href="https://flic.kr/s/aHBqjABTUH">Spring Daze 2023 (Partner Event)</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjABTUH">
+                Spring Daze 2023 (Partner Event)
+              </a>
             </li>
             <li>
-              <a href="https://flic.kr/s/aHBqjABSCR">Sterling International Festival 2023 (Partner Event)</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjABSCR">
+                Sterling International Festival 2023 (Partner Event)
+              </a>
             </li>
             <li>
-              <a href="https://flic.kr/s/aHBqjAvSQK">Basant Bahar 2023 Pictures</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjAvSQK">
+                Basant Bahar 2023 Pictures
+              </a>
             </li>
             <li>
-              <a href="https://www.flickr.com/gp/155699555@N08/rMVgdHP0n6">Youth Achievement Award Luncheon 2022</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/gp/155699555@N08/rMVgdHP0n6">
+                Youth Achievement Award Luncheon 2022
+              </a>
             </li>
             <li>
-              <a href="https://flic.kr/s/aHBqjAbHT1">Hum Sub Diwali 2022 (Set 1)- All Pictures</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjAbHT1">
+                Hum Sub Diwali 2022 (Set 1)- All Pictures
+              </a>
             </li>
             <li>Hum Sub Diwali 2022 (Set 2)</li>
             <ul>
               <li>
-                <a href="https://flic.kr/s/aHBqjAbLQJ">Hum Sub Diwali 2022 - Morning Segment</a>
+                <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjAbLQJ">
+                  Hum Sub Diwali 2022 - Morning Segment
+                </a>
               </li>
               <li>
-                <a href="https://flic.kr/s/aHBqjAbLS2">Hum Sub Diwali 2022 - Evening Segment</a>
+                <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjAbLS2">
+                  Hum Sub Diwali 2022 - Evening Segment
+                </a>
               </li>
             </ul>
             <li>
-              <a href="https://flic.kr/s/aHBqjACK3D">Indian Culture Learning Series 2022</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjACK3D">
+                Indian Culture Learning Series 2022
+              </a>
             </li>
             <li>
-              <a href="https://flic.kr/s/aHBqjzGxgu">Basant Bahar 2022 Pictures</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHBqjzGxgu">
+                Basant Bahar 2022 Pictures
+              </a>
             </li>
             <li>
-              <a href="https://www.flickr.com/gp/155699555@N08/sQXd20">Cary Diwali 2021 Pictures</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/gp/155699555@N08/sQXd20">
+                Cary Diwali 2021 Pictures
+              </a>
             </li>
             <li>
-              <a href="https://www.flickr.com/photos/155699555@N08/sets/72157711624058531">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.flickr.com/photos/155699555@N08/sets/72157711624058531"
+              >
                 2019 Youth Achievement Award Luncheon
               </a>
             </li>
             <li>
-              <a href="https://www.flickr.com/photos/155699555@N08/sets/72157711624058531">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.flickr.com/photos/155699555@N08/sets/72157711624058531"
+              >
                 2019 Youth Achievement Award Luncheon
               </a>
             </li>
             <li className="">
-              <a href="https://flic.kr/s/aHsmHMUWxx">Cary Diwali 2019 Pictures</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://flic.kr/s/aHsmHMUWxx">
+                Cary Diwali 2019 Pictures
+              </a>
             </li>
             <li className="">
-              <a href="https://www.flickr.com/gp/155699555@N08/B690SZ">Basant Bahar 2019 pictures</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/gp/155699555@N08/B690SZ">
+                Basant Bahar 2019 pictures
+              </a>
             </li>
             <li>
-              <a href="https://www.flickr.com/photos/155699555@N08/albums/72157673134563367">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.flickr.com/photos/155699555@N08/albums/72157673134563367"
+              >
                 2018 Youth Achievement Award Luncheon Pics
               </a>
             </li>
             <li>
-              <a href="https://www.flickr.com/gp/155699555@N08/ZxeM2b">Cary Diwali 2018 Pics</a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/gp/155699555@N08/ZxeM2b">
+                Cary Diwali 2018 Pics
+              </a>
             </li>
             <li>
-              <a href="https://www.flickr.com/photos/155699555@N08/albums/72157693950842184">Basant Bahar 2018 Pics</a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.flickr.com/photos/155699555@N08/albums/72157693950842184"
+              >
+                Basant Bahar 2018 Pics
+              </a>
             </li>
             <li>
-              <a href="https://www.flickr.com/photos/155457582@N04/albums/72157688152144823">Cary Diwali 2017 Pics</a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.flickr.com/photos/155457582@N04/albums/72157688152144823"
+              >
+                Cary Diwali 2017 Pics
+              </a>
             </li>
           </ul>
         </TabsContent>
