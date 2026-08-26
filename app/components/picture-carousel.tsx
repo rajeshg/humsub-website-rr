c36ac1719601bc6b9371aacb92534e099ac93a35
Rajesh Gollapudi <rajesh.gollapudi@3ds.com>
2026-08-26 12:30:36 -0400

    chore: update carousel images for 2026

--- app/components/picture-carousel.tsx
+++ app/components/picture-carousel.tsx
@@ -2,20 +2,17 @@ import ImageCarousel from "./image-carousel"
 
 export function PictureCarousel() {
   const images = [
-    "/assets/home-carousel/basantbahar24-0303-160300fin2.webp",
-    "/assets/home-carousel/basantbahar24-0303-162119fin2.webp",
-    "/assets/home-carousel/basantbahar24-0303-170353fin2.webp",
-    "/assets/home-carousel/hum-sab-diwali-218.webp",
-    "/assets/home-carousel/hum-sab-diwali-252.webp",
-    "/assets/home-carousel/hum-sab-diwali-282.webp",
+    { src: "/assets/home-carousel/bb2026-1.jpg", alt: "Basant Bahar 2026" },
+    { src: "/assets/home-carousel/bb2026-2.jpg", alt: "Basant Bahar 2026" },
+    { src: "/assets/home-carousel/bb2026-3.jpg", alt: "Basant Bahar 2026" },
+    { src: "/assets/home-carousel/bb2026-4.jpg", alt: "Basant Bahar 2026" },
+    { src: "/assets/home-carousel/hd2025-1.jpg", alt: "Diwali 2025" },
+    { src: "/assets/home-carousel/hd2025-2.jpg", alt: "Diwali 2025" },
+    { src: "/assets/home-carousel/hd2025-3.jpg", alt: "Diwali 2025" },
+    { src: "/assets/home-carousel/hd2025-4.jpg", alt: "Diwali 2025" },
+    { src: "/assets/home-carousel/bb2024-1.webp", alt: "Basant Bahar 2024" },
+    { src: "/assets/home-carousel/hd2024-1.webp", alt: "Diwali 2024" },
   ]
 
-  return (
-    <ImageCarousel
-      slides={images.map((src) => ({
-        src,
-        alt: "event pic",
-      }))}
-    />
-  )
+  return <ImageCarousel slides={images} />
 }
