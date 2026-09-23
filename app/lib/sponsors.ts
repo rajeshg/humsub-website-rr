7440093ebf6496f8e600c445d286a45fe07830c0
Rajesh Gollapudi <rajesh.gollapudi@3ds.com>
2026-09-22 22:23:46 -0400

    chore: remove CRS from sponsors page

--- app/lib/sponsors.ts
+++ app/lib/sponsors.ts
@@ -66,14 +66,6 @@ export const sponsors: Sponsor[] = [
     level: "gold",
     href: "https://www.pnc.com/",
   },
-  {
-    name: "CRS Marble & Granite",
-    imagePath: "/assets/sponsors/logo_crs.jpg",
-    level: "gold",
-    description: "Exclusive sponsor for Shuttle Service",
-    href: "https://crsgranite.com/",
-    label: "Shuttle Sponsor",
-  },
   // Silver Sponsors
   {
     name: "First Bank",
